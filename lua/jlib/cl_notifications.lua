5384d6fa95b48e01507684525bd692060195e9b7
Senior Black <devidbf3@gmail.com>
2022-06-22 09:08:59 +0300

    Converted jlib messages to jNet

--- lua/jlib/cl_notifications.lua
+++ lua/jlib/cl_notifications.lua
@@ -2,13 +2,8 @@
 --  Realm:      Client
 --  Purpose:    Allows convinient networking of messages from server to client.
 --  Date:       07/03/2021 - 1:44 PM
-local net_Receive = net.Receive
-local net_ReadTable = net.ReadTable
 local unpack = unpack
-local ipairs = ipairs
-local type = type
 
-net_Receive("jlib.notifications", function()
-    local tab = net_ReadTable()
-    chat.AddText(unpack(tab))
+jnet.subscribe("message", function(buffer)
+    chat.AddText(unpack(buffer))
 end)
